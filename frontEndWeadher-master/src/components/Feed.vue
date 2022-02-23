f6414c27009bd89d18ce341db8942ea44d9ed9c3
Ghada Dhif <ghada1edhif@gmail.com>
2022-02-23 09:16:06 +0100

    create feed interface

---
+++ frontEndWeadher-master/src/components/Feed.vue
@@ -0,0 +1,93 @@
+/* eslint-disable */
+
+<template>
+  <div id="app">
+    <main>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            <p>City Name</p>
+          </div>
+          <div class="date">
+           date
+          </div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">temperature°F</div>
+          <div class="weather">weather</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Feed',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "montserrat", sans-serif;
+}
+#app {
+  background-image: url("../assets/background.jpeg");
+  background-size: cover;
+  background-position: center;
+  transition: 0.4s;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  font-style: italic;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
